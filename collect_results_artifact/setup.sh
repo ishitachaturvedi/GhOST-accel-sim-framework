45e11c2a7f559a09fb5dba0c34dc10caff258535
yucan <yucan.wu@princeton.edu>
2024-03-26 01:07:43 -0400

    Updated setup script

--- collect_results_artifact/setup.sh
+++ collect_results_artifact/setup.sh
@@ -1,7 +1,3 @@
-# get git folder
-git clone --recurse-submodules https://github.com/ishitachaturvedi/GhOST-accel-sim-framework/
-cd GhOST-accel-sim-framework
-
 # set and test our main path, this needs to be done everytime
 export ACCEL_SIM_DIR=`pwd`
 # test the path [optional]
@@ -11,15 +7,12 @@ export ACCEL_SIM_DIR=`pwd`
 tar -xzf gpu-simulator-LOOG.tar.gz &
 
 # load SASS traces [zenode link]
-wget "http://dl.dropboxusercontent.com/scl/fi/mojmdrllvv04ybbdnjp8n/GhOST_SASS_traces.tar.gz?rlkey=ij27ex7mrjnhybo53oossetv6&dl=0" -O GhOST_SASS_traces.tar.gz
 tar -xzf GhOST_SASS_traces.tar.gz & 
 
 # load area calculation [zenode link]
-wget "http://dl.dropboxusercontent.com/scl/fi/hbrthlutldyvyge2u205v/GhOST-area-calculation.tar.gz?rlkey=e6kli4ojfeqkeuytysy6ybc0f&dl=0" -O GhOST-area-calculation.tar.gz
 tar -xzf GhOST-area-calculation.tar.gz &
 
 # load image file [zenode link]
-wget "http://dl.dropboxusercontent.com/scl/fi/w39fuxejxja1y8f8ciog8/gpu-accel-sim-docker.img?rlkey=zqbgaua0xdiq7tc4zr2lk42c1&dl=0" -O gpu-accel-sim-docker.img &
 
 # Wait for all background jobs to finish
 echo "Wait for all background jobs to finish ..." >&2
